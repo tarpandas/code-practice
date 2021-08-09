4e7aea85006bebac46d3a9ea579b695079a7a9e5
Tarpan Das <61510889+tarpandas@users.noreply.github.com>
2021-08-09 22:55:36 +0530

    Add files via upload

---
+++ Data Structures/8_Singly_Linked_List.c
@@ -0,0 +1,44 @@
+//Singly Linked List
+#include<stdio.h>
+#include<stdlib.h>
+void new_node(int);
+void delete(int);
+void display();
+typedef struct node{
+    int data;
+    struct node * next;
+}node;
+
+int main(){
+    new_node(12);
+    new_node(23);
+    new_node(34);
+    new_node(22);
+    display();
+    return 0;
+}
+
+node * start = NULL;
+void new_node(int item){
+    node * ptr = NULL;
+    ptr = (node *)malloc(sizeof(node));
+    ptr->data=item;
+    if(start==NULL){
+        ptr->next=NULL;
+        start = ptr;
+    }
+    else{
+        ptr->next=start;
+        start=ptr;
+    }
+}
+void display(){
+    node *ptr=start;
+    while(ptr->next!=NULL){
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }if(ptr->next==NULL)
+        printf("%d",ptr->data);
+}
+
+
